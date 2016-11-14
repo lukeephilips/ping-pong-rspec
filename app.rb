51f6fec69f5c8b083be064020263badeff367ca9
Luke Philips and Tracie Weitzman <tracie@>
2016-11-14 10:24:42 -0800

    connect method to sinatra to view

--- app.rb
+++ app.rb
@@ -2,12 +2,35 @@ require('sinatra')
 require('sinatra/reloader')
 require('pry')
 
+# class String
+#   define_method(:ping_pong) do
+#     number = self.to_i().abs()
+#     numberArray = []
+#     (number+1).times() do |number|
+#       if number != 0
+#         if number % 15 == 0
+#           numberArray.push('ping-pong')
+#         elsif number % 3 == 0
+#           numberArray.push('ping')
+#         elsif number % 5 == 0
+#           numberArray.push('pong')
+#         else
+#           numberArray.push(number)
+#         end
+#       else
+#       numberArray.push(number)
+#       end
+#     end
+#     numberArray
+#   end
+# end
 
 get('/') do
   erb(:form)
 end
 
-get('/ping_pong') do
+get('/ping_pong_output') do
   @number = params.fetch('userNumber')
-  erb(:ping_pong)
+  @outputArray = @number.ping_pong()
+  erb(:ping_pong_output)
 end
