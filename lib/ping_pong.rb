f7c5aa71ce99ed3b712f1fed1501f2c97f4b6fb7
Luke Philips and Tracie Weitzman <tracie@>
2016-11-14 10:07:45 -0800

    converts multiples of 15 to ping-pong

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,10 +3,18 @@ class String
     number = self.to_i().abs()
     numberArray = []
     (number+1).times() do |number|
-      if number % 3 == 0 && number != 0
-        numberArray.push('ping')
+      if number != 0
+        if number % 15 == 0
+          numberArray.push('ping-pong')
+        elsif number % 3 == 0
+          numberArray.push('ping')
+        elsif number % 5 == 0
+          numberArray.push('pong')
+        else
+          numberArray.push(number)
+        end
       else
-        numberArray.push(number)
+      numberArray.push(number)
       end
     end
     numberArray
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -18,4 +18,10 @@ describe('String#ping_pong') do
   it("converts multiples of 3 to 'ping'") do
     expect(("3").ping_pong()).to(eq([0,1,2,'ping']))
   end
+  it("converts multiples of 5 to 'pong'") do
+    expect(("5").ping_pong()).to(eq([0,1,2,'ping',4,'pong']))
+  end
+  it("converts multiples of 15 to 'ping-pong'") do
+    expect(("15").ping_pong()).to(eq([0,1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',13,14,'ping-pong']))
+  end
 end
